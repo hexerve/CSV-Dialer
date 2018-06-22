625cd306e4263d23cb1a6aa0b1fdd4e7499f1e55
hacks2018 <adityaagarwal505@gmail.com>
2018-06-22 18:59:27 +0530

    AddProject

--- app/src/main/java/com/example/acer/callingapp/MainActivity.java
+++ app/src/main/java/com/example/acer/callingapp/MainActivity.java
@@ -5,9 +5,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.net.Uri;
 
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +19,7 @@ import android.view.View;
 import android.widget.Button;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -150,6 +153,16 @@ public class MainActivity extends AppCompatActivity {
         Delay();
 
         mBrowse = (Button) findViewById(R.id.browse);
+        mBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("text/csv");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),RESULT_LOAD_IMAGE);
+                Log.i("helloo","asgsa");
+            }
+        });
 
 //        log();
 //        getCallDetails();
@@ -178,6 +191,22 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("MyActivity", "asda");
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+//            Uri uri =data.getData();
+//            try {
+//                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+//                imageView.setImageBitmap(bitmap);
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
     @Override
     protected void onResume() {
