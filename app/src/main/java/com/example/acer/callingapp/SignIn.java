dc6a236f30778f7d9f5643f5e674a95271c7be87
hacks2018 <adityaagarwal505@gmail.com>
2018-06-26 17:06:34 +0530

    AddProject

---
+++ app/src/main/java/com/example/acer/callingapp/SignIn.java
@@ -0,0 +1,20 @@
+package com.example.acer.callingapp;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+    }
+}
