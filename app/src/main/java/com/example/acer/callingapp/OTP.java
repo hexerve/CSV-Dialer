e74b19f482e6ee6f102c66fc829063a0b00b241d
hacks2018 <adityaagarwal505@gmail.com>
2018-06-27 16:40:00 +0530

    AddProject

--- app/src/main/java/com/example/acer/callingapp/OTP.java
+++ app/src/main/java/com/example/acer/callingapp/OTP.java
@@ -1,13 +1,58 @@
 package com.example.acer.callingapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class OTP extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otp);
+
+    public static final String INTENT_PHONENUMBER = "phonenumber";
+
+     static String mPhoneNumber;
+     static String mCountryIso = "+91";
+
+     void tryAndPrefillPhoneNumber(String phone) {
+        mPhoneNumber =phone;
+        if (checkCallingOrSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+            TelephonyManager manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+//            mPhoneNumber.setText(manager.getLine1Number());
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 0);
+        }
+        onButtonClicked();
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            tryAndPrefillPhoneNumber(mPhoneNumber);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+                Toast.makeText(this, "This application needs permission to read your phone number to automatically "
+                        + "pre-fill it", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void openActivity(String phoneNumber) {
+        Intent verification = new Intent(this, VerificationActivity.class);
+        verification.putExtra(INTENT_PHONENUMBER, mCountryIso+phoneNumber);
+        startActivity(verification);
+    }
+
+    public void onButtonClicked() {
+        openActivity(getE164Number());
+    }
+
+    private String getE164Number() {
+        return mPhoneNumber;
     }
 }
--- app/src/main/java/com/example/acer/callingapp/SignIn.java
+++ app/src/main/java/com/example/acer/callingapp/SignIn.java
@@ -1,19 +1,67 @@
 package com.example.acer.callingapp;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
 
-public class SignIn extends AppCompatActivity {
+import static com.example.acer.callingapp.R.id.phone;
 
+public class SignIn extends AppCompatActivity {
+    String emaill,passs;
+    EditText pass,email;
+    Button signIn;
+    int count = 0;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        signIn = (Button) findViewById(R.id.btn_signIn);
+        pass = (EditText) findViewById(R.id.passIn);
+        email = (EditText) findViewById(R.id.emailIn);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(SignIn.this,SignUp.class);
+                startActivity(homeIntent);
+            }
+        });
+
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                emaill = pass.getText().toString();
+                passs = email.getText().toString();
+
+                count=0;
+                if (emaill.equals("")) {
+                    count=10;
+                    email.setError("Enter Valid Name");
+                }
+                if (passs.equals("")) {
+                    count=10;
+                    pass.setError("Enter Valid password");
+                }
+                if (count==0){
+                    Intent homeIntent = new Intent(SignIn.this,MainActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }
+        });
 
 
     }
